69c7967d5646e5fbd90a23deac68b23135d7def8
phantom1407 <marialecisnerosh@gmail.com>
2026-09-03 10:04:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import Image
 from googletrans import Translator
 
 st.title('Análisis de Sentimiento')
-image = Image.open('emoticones.jpg')
+image = Image.open('feelings.jpg')
 st.image(image)
 st.subheader("Por favor escribe en el campo de texto la frase que deseas analizar")
 
